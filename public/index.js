f53feb77b6d2ea116853b1e43f68daba13be4a28
ChaoHong1944 <91182144+ChaoHong1944@users.noreply.github.com>
2024-11-13 23:00:04 -0500

    Revert "Front end progress"
    This reverts commit 4d84f6a11b709eba43472fbec90298132b2296a1.

--- public/index.js
+++
@@ -1,5 +0,0 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiY2hhb2hvbmcxOTQ0IiwiYSI6ImNtM2dwb3FmejA3cmQyam9ta2MzMDAwYTkifQ.q4S2Z_nEPR2ebVIXPDg9uw';
-    var map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11'
-    });
