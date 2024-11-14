28f0c986b259cdd4cb896bf6842146328ebb4963
ChaoHong1944 <91182144+ChaoHong1944@users.noreply.github.com>
2024-11-13 23:29:56 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,22 @@
-import { useState } from 'react'
-import ReactMapGL, { Marker, Popup } from "react-map-gl"
+import React from 'react';
+import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps"
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Map() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React cheese</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <GoogleMap 
+      defaultZoom={10} 
+      defaultCenter={{ lat: 40.712776, lng: -74.005974 }}
+   />
   )
 }
 
-export default App
+const WrappedMap =  withScriptjs(withGoogleMap(Map));
+
+export default function App(){
+  return <div> 
+    <WrappedMap googleMapURL={''}/>
+  </div>;
+}
